3504e96fc0a6ff5bb3e665a5cc5cb284c2d058cc
Jonas Addicks <jonas.addicks@gmx.de>
2024-05-04 19:23:46 +0200

    client base test

--- src/main/java/GKAClient.java
+++ src/main/java/GKAClient.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class GKAClient {
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import java.util.Scanner;
+
+public class GKAClient {
+
+    public static void main(String[] args) {
+        System.setProperty("org.graphstream.ui", "swing");
+
+        Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.display();
+
+        Scanner scanner = new Scanner(System.in);
+        String nextLine;
+        while (scanner.hasNext() | nextLine == null || !nextLine.equals("quit")) {
+            nextLine = scanner.next();
+
+            //graph.addEdge()
+        }
+    }
 }
